7c0153fe12607a0eb0cfde3ddc5c92c490c076c6
ndkai <darkspirit909@gmail.com>
2024-11-21 23:36:47 +0700

    feat: day ui

--- lib/main.dart
+++ lib/main.dart
@@ -24,43 +24,65 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: Scaffold(
+          backgroundColor: Colors.black,
           body: Center(
             child: LayoutBuilder(
               builder: (context, constraints) {
                 // Get the screen dimensions
                 double screenWidth = constraints.maxWidth;
                 double screenHeight = constraints.maxHeight;
-                double scaleFactor = 1.5;
-
+                double scaleFactor = 2.5;
+                double width = screenWidth * 0.55;
+                double height = width / scaleFactor;
+                double padding = width * (12.5 / 369);
                 return Center(
-                    child: SizedBox(
-                  height: screenHeight * 0.8,
-
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(60),
-                        child: Container(
-                          height: 130,
-                          width: screenWidth * 0.2,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                          ),
-                          child: Stack(
-                            children: [
-
-                              Positioned(child: SvgPicture.asset(Assets.dayClouds),right: -10, bottom: -10,),
-                              Positioned(child: SvgPicture.asset(Assets.dayCloudsbacks), right: -10,),
-                              Positioned(child: SvgPicture.asset(Assets.dayRays), left: -10,),
-                              Padding(padding: EdgeInsets.all(8),child: SvgPicture.asset(Assets.daySun),),
-
-                            ],
-                          ),
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(999),
+                      child: Container(
+                        height: height,
+                        width: width,
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
                         ),
-                      )
-                    ],
-                  ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              bottom: 0,
+                              left: padding,
+                              child: SvgPicture.asset(Assets.dayClouds,
+                                  width: width * (386 / 369)),
+                            ),
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              child: SvgPicture.asset(
+                                Assets.dayCloudsbacks,
+                                width: width * (377 / 369),
+                              ),
+                            ),
+                            Positioned(
+                              left: 0,
+                              top: 0,
+                              child: SvgPicture.asset(
+                                Assets.dayRays,
+                                height: height,
+                              ),
+                            ),
+                            Positioned(
+                                left: padding,
+                                top: padding,
+                                child: SvgPicture.asset(
+                                  Assets.daySun,
+                                  height: height * 120 / 145,
+                                )),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ));
               },
             ),
